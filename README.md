7235b9d624bd2c80cd3aae34e81d5929e1ca026f
Athilas-Silva <athilas.soares@hotmail.com>
2021-07-10 19:21:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nlw6-discover
+ Projeto desenvolvido na NLW06
---
+++ src/db/config.js
@@ -0,0 +1,9 @@
+// Importando os modulos do banco de dados
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite")
+
+module.exports = () => 
+    open({
+        filename: './src/db/rocketq.sqlite',
+        driver: sqlite3.Database,
+    });
